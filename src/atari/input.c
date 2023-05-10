f3e64ea2fe3e42688a68f2d8405f4c1ef160f75e
diggerbonk <14832259+diggerbonk@users.noreply.github.com>
2023-05-10 07:50:50 -0600

    Revert "removed intput_line method no longer used in atari or shared code"
    This reverts commit 5a3c0549dd2729fdf1ca44fd8cf259aa15d2ed5b.

--- src/atari/input.c
+++ src/atari/input.c
@@ -86,6 +86,10 @@ unsigned char input_handle_joystick(void)
     return 0;
 }
 
+void input_line(unsigned char x, unsigned char y, unsigned char o, char *c, unsigned char l, bool password)
+{
+}
+
 void input_line_set_wifi_custom(char *c)
 {
   bar_show(20);
