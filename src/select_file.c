10e3ccbe3a16a3b0624e2546753f8232b3a456e2
diggerbonk <14832259+diggerbonk@users.noreply.github.com>
2025-02-02 07:22:18 -0700

    test

--- src/select_file.c
+++ src/select_file.c
@@ -320,6 +320,7 @@ void select_file_devance(void)
   sf_subState = SF_DISPLAY; // And display the result.
 }
 
+// beep
 unsigned char select_file_is_folder(void)
 {
   unsigned char result = select_file_entry_type();
